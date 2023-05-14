06a0e61221f84faeed13872afd6b116280d2d42f
AMG3141 <67506477+AMG3141@users.noreply.github.com>
2023-05-14 18:08:24 +0100

    adding enemies

--- script.js
+++ script.js
@@ -135,21 +135,26 @@ window.addEventListener("load", function() {
 				context.stroke();
 			}
 		}
+
+		update() {
+
+		}
 	}
 
 	class Egg {
 		constructor(game) {
 			this.game = game;
-			this.collisionX = Math.random() * this.game.width;
-			this.collisionY = Math.random() * this.game.height;
 			this.collisionRadius = 40;
+			this.margin = this.collisionRadius * 2;
+			this.collisionX = this.margin +  Math.random() * (this.game.width - this.margin * 2);
+			this.collisionY = this.game.topMargin + Math.random() * (this.game.height - this.game.topMargin - this.margin);
 			this.image = document.getElementById("egg");
 			this.spriteWidth = 110;
 			this.spriteHeight =135;
 			this.width = this.spriteWidth;
 			this.height = this.spriteHeight;
-			this.spriteX = this.collisionX - this.width / 2;
-			this.spriteY = this.collisionY - this.height / 2;
+			this.spriteX;
+			this.spriteY;
 		}
 
 		draw(context) {
@@ -162,6 +167,62 @@ window.addEventListener("load", function() {
 				context.fill();
 				context.restore();
 				context.stroke();
+			}
+		}
+
+		update() {
+			this.spriteX = this.collisionX - this.width / 2;
+			this.spriteY = this.collisionY - this.height / 2 - 30;
+
+			let collisionObjects = [this.game.player, ...this.game.obstacles]; // ... is spread operator (expands the elements in the array)
+			collisionObjects.forEach(collisionObject => {
+				let [ collision, distance, sumOfRadii, dx, dy ] = this.game.checkCollision(this, collisionObject);
+				if (collision) {
+					const unit_x = dx / distance;
+					const unit_y = dy / distance;
+					this.collisionX = collisionObject.collisionX + (sumOfRadii + 1) * unit_x;
+					this.collisionY = collisionObject.collisionY + (sumOfRadii + 1) * unit_y;
+				}
+			});
+		}
+	}
+
+	class Enemy {
+		constructor(game) {
+			this.game = game;
+			this.collisionRadius = 30;
+			this.collisionX = this.game.width;
+			this.collisionY = Math.random() * this.game.height;
+			this.speedX = Math.random() * 3 + 0.5;
+			this.image = document.getElementById("toad")
+			this.spriteWidth = 140;
+			this.spriteHeight = 260;
+			this.width = this.spriteWidth;
+			this.height = this.spriteHeight;
+			this.spriteX;
+			this.spriteY;
+		}
+
+		draw(context) {
+			context.drawImage(this.image, this.spriteX, this.spriteY);
+
+			if (this.game.debug) {
+				context.beginPath();
+				context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+				context.save();
+				context.globalAlpha = 0.5;
+				context.fill();
+				context.restore();
+				context.stroke();
+			}
+		}
+
+		update() {
+			this.collisionX -= this.speedX;
+
+			if (this.spriteX + this.width < 0) {
+				this.collisionX = this.game.width;
+				this.collisionY = Math.random() * this.game.height;
 			}
 		}
 	}
@@ -178,6 +239,8 @@ window.addEventListener("load", function() {
 			this.obstacles = [];
 			this.maxEggs = 10;
 			this.eggs = [];
+			this.gameObjects = [];
+			this.enemies = [];
 
 			this.fps = 70;
 			this.timer = 0;
@@ -219,11 +282,19 @@ window.addEventListener("load", function() {
 		
 		render(context, deltaTime) {
 			if (this.timer > this.interval) {
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-				this.obstacles.forEach(obstacle => obstacle.draw(context))
-				this.eggs.forEach(egg => egg.draw(context))
-				this.player.draw(context);
-				this.player.update();
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				this.gameObjects = [ ...this.eggs, ...this.obstacles, this.player ];
+
+				// Sort the game objects by their vertical position
+				this.gameObjects.sort((a, b) => {
+					return a.collisionY - b.collisionY;
+				});
+				
+				this.gameObjects.forEach(object => {
+					object.draw(context);
+					object.update();
+				});
+
 				this.timer = 0;
 			}
 
@@ -250,7 +321,15 @@ window.addEventListener("load", function() {
 			this.eggs.push(new Egg(this));
 		}
 
+		addEnemy() {
+			this.enemies.push(new Enemy(this));
+		}
+
 		init() {
+			for (let i = 0; i < 3; i++ ) {
+				this.addEnemy();
+			}
+
 			// Brute force algorithm to pack the circles such that they don't overlap, and have appropriate spacing between each other and the edges
 			let attempts = 0;
 			while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
