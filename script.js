35241f1bef1bc8ccdfd7fe45c082a1c5f2e86210
AMG3141 <67506477+AMG3141@users.noreply.github.com>
2023-05-17 15:17:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-import { Egg } from "./classes/Egg.js";
-import { Enemy } from "./classes/Enemy.js";
-import { Firefly } from "./classes/Firefly.js";
 import { Game } from "./classes/Game.js";
-import { Larva } from "./classes/Larva.js";
-import { Obstacle } from "./classes/Obstacle.js";
-import { Particle } from "./classes/Particle.js";
-import { Player } from "./classes/Player.js";
-import { Spark } from "./classes/Spark.js";
 
 window.addEventListener("load", function() {
 	// Set the canvas size to be the same as the image size to prevent distortion
-	const canvas = document.getElementById("canvas1");
+	const canvas = document.createElement("canvas1");
+	// const canvas = document.getElementById("canvas1");
 	const ctx = canvas.getContext("2d")
 	canvas.width = 1280;
 	canvas.height = 720;
